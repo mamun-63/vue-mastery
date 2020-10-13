673d6ed76f8fbde2e8b1e3006c3ffe53f6d04002
Mamun <mamun.cse63@gmail.com>
2020-10-14 04:06:17 +0600

    5 Event Handling

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var app = new Vue({
   el: '#app',
+
   data: {
     product: 'Socks',
     image: './assets/vmSocks-green-onWhite.jpg',
@@ -9,11 +10,23 @@ var app = new Vue({
       {
         variantId: 2234,
         variantColor: 'green',
+        variantImage: './assets/vmSocks-green-onWhite.jpg',
       },
       {
         variantId: 2235,
         variantColor: 'blue',
+        variantImage: './assets/vmSocks-blue-onWhite.jpg',
       },
     ],
+    cart: 0,
+  },
+
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    updateImage(variantImage) {
+      this.image = variantImage
+    },
   },
 })
