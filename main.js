1fc206294574abd337da329a4ad098fc64b87d7a
Mamun <mamun.cse63@gmail.com>
2020-10-14 05:28:48 +0600

    7 Computed Properties

--- main.js
+++ main.js
@@ -2,31 +2,52 @@ var app = new Vue({
   el: '#app',
 
   data: {
+    brand: 'Vue Mastery',
     product: 'Socks',
-    image: './assets/vmSocks-green-onWhite.jpg',
-    inStock: false,
+    selectedVariant: 0,
     details: ['80% cotton', '20% polyster', 'Gender-neutral'],
     variants: [
       {
         variantId: 2234,
         variantColor: 'green',
         variantImage: './assets/vmSocks-green-onWhite.jpg',
+        variantQuantity: 10,
       },
       {
         variantId: 2235,
         variantColor: 'blue',
         variantImage: './assets/vmSocks-blue-onWhite.jpg',
+        variantQuantity: 0,
       },
     ],
     cart: 0,
+    onSale: false, // challenge task just
   },
 
   methods: {
     addToCart() {
       this.cart += 1
     },
-    updateImage(variantImage) {
-      this.image = variantImage
+    updateImage(index) {
+      this.selectedVariant = index
+      console.log(index)
+    },
+  },
+
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity
+    },
+    sale() {
+      return this.onSale
+        ? this.brand + ' ' + this.product + ' are on sale'
+        : this.brand + ' ' + this.product + ' are not on sale'
     },
   },
 })
