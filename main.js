be828e3aeeb7b09c21653ff64b407f2a1f5df521
Mamun <mamun.cse63@gmail.com>
2020-10-14 03:38:13 +0600

    4 List Rendering

--- main.js
+++ main.js
@@ -3,8 +3,17 @@ var app = new Vue({
   data: {
     product: 'Socks',
     image: './assets/vmSocks-green-onWhite.jpg',
-    inventory: 8,
-    display: true,
-    onSale: true,
+    inStock: true,
+    details: ['80% cotton', '20% polyster', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue',
+      },
+    ],
   },
 })
